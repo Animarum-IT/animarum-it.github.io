0d64912c9868803b0d45fc1d1e1cae688f7088f3
Hady Nourallah <hady.nourallah@gmail.com>
2024-09-16 14:41:49 +0200

    update assets basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    assetPrefix: './',
+    basePath: "/animarum-web",
 
     images: {
         unoptimized: true,
